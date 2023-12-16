d2063795e8b7edf3e0d507d5af1febdd419ddef7
harrison <harrison@suade.org>
2023-12-16 14:49:07 +0000

    test init

--- src/test_dataset_builder.py
+++ src/test_dataset_builder.py
@@ -1,12 +1,12 @@
 from feynman_dataset_builder import FeynmanGraph
 
-graph = FeynmanGraph(5)
-graph.add_edge(1, 2)
-graph.add_edge(2, 3)
-graph.add_edge(3, 1)
-graph.add_edge(1,4)
-graph.connect_global_node()
-graph.undirected()
+graph = FeynmanGraph(edges=[(1,2),(2,3),(2,4),(4,5),(4,6)])
+
+# graph.connect_global_node()
+graph.make_edges_undirected()
+
+for i in range(1,7):
+    graph.add_node_feat(str(i), [1,0,0])
 
 print(graph.edge_index)
 graph.build_dfs()
